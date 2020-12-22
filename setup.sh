7b71c2c62f8144686a87b85505648acc4e709f88
Nathan Shelly <nsnshelly@gmail.com>
2020-12-21 20:30:23 -0600

    test through flakes w/o recreating channels

--- setup.sh
+++ setup.sh
@@ -13,10 +13,6 @@ echo "Nix installed"
 # initialize `nix` for next steps - same line added to /etc/zshrc by installer
 . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 
-# TODO: avoid needing to recreate channels owned by root
-sudo rm -rf ~/.nix-defexpr
-nix-channel --update
-
 nix-build \
   https://github.com/LnL7/nix-darwin/archive/master.tar.gz \
   -A installer > /dev/null
@@ -27,3 +23,23 @@ else
   echo '`nix-darwin` installer failed. Please fix any issues and try again.'
   exit 1
 fi
+
+printf "\n\nnix-darwin setup complete\n\n"
+
+# make just installed `darwin-rebuild` command available in current script
+[ -f /etc/static/bashrc ] && source /etc/static/bashrc
+
+# move nix.conf file so it can be recreated and subsequently owned by nix-darwin
+if [[ -f /etc/nix/nix.conf ]] && ! grep --quiet flakes < /etc/nix/nix.conf; then
+  sudo mv /etc/nix/nix.conf /etc/nix/nix.conf.bak
+fi
+
+printf "\n\nbuilding first darwin generation\n\n"
+darwin-rebuild switch -I darwin-config="./darwin.nix"
+
+printf "\n\nNIX version: %s\n\n" "$(nix --version)"
+
+printf "\n\nbuilding w/ flakes\n\n"
+darwin-rebuild switch --flake ".#default" --impure --show-trace
+
+echo "flakes setup"
